b053de1ae27c91b2ba2ef00272d23d46fa8f6134
Paulina <pk.koziol49@gmail.com>
2018-01-13 14:52:01 +0100

    extracting RegisterPanel intro separate file

--- basic/src/main/js/app.js
+++ basic/src/main/js/app.js
@@ -1,14 +1,11 @@
 'use strict';
 
-// tag::vars[]
 const React = require('react');
 const ReactDOM = require('react-dom');
 const client = require('./client');
+const RegisterPanel = require('./RegisterPanel').registerPanel;
 import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 
-// end::vars[]
-
-// tag::app[]
 const App = () => (
     <Switch>
         <Route exact path='/' component={RegisterPanel}/>
@@ -16,14 +13,12 @@ const App = () => (
         </Route>
         <Home>
             <Switch>
-                <Route path='/view2' component={() => <div>yo!</div>}/>
-                <Route path='/view1' component={() => <div>yababaa!</div>}/>
+                <Route path='/view2' component={View2}/>
+                <Route path='/view1' component={View1}/>
             </Switch>
         </Home>
     </Switch>
 );
-
-// end::app[]
 
 class Header extends React.Component{
 
@@ -88,117 +83,6 @@ class LoginPanel extends React.Component{
     render(){
         return (
             <div>test login</div>
-        )
-    }
-}
-
-class RegisterPanel extends React.Component{
-
-    constructor(props) {
-        super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleSubmit(e) {
-        e.preventDefault();
-        var newEmployee = {};
-        // this.props.attributes.forEach(attribute => {
-        //     newEmployee[attribute] = ReactDOM.findDOMNode(this.refs[attribute]).value.trim();
-        // });
-        newEmployee["name"] = ReactDOM.findDOMNode(this.refs["name"]).value.trim();
-        newEmployee["email"] = ReactDOM.findDOMNode(this.refs["email"]).value.trim();
-        newEmployee["username"] = ReactDOM.findDOMNode(this.refs["username"]).value.trim();
-        newEmployee["password"] = ReactDOM.findDOMNode(this.refs["password"]).value.trim();
-
-        client({
-            method: 'POST',
-            path: '/users/register',
-            entity: newEmployee,
-            headers: {'Content-Type': 'application/json'}
-        }).done(response => {
-            console.log(response);
-            if (response.status.code === 200) {
-                this.props.history.push('/home');
-            }
-        });
-
-        console.log(newEmployee);
-        // Navigate away from the dialog to hide it.
-        // window.location = "#";
-    }
-
-
-    render() {
-
-        return (
-            <div className="container">
-                <div className="row main">
-                    <div className="panel-heading">
-                        <div className="panel-title text-center">
-                            <h1 className="title">Lunch Roulette</h1>
-                            <hr />
-                        </div>
-                    </div>
-                    <div className="main-login main-center">
-                        <form className="form-horizontal" method="post" action="#">
-
-                            <div className="form-group">
-                                <label htmlFor="name" className="cols-sm-2 control-label">Your Name</label>
-                                <div className="cols-sm-10">
-                                    <div className="input-group">
-                                        <span className="input-group-addon"><i className="fa fa-user fa" aria-hidden="true"></i></span>
-                                        <input type="text" className="form-control" name="name" ref="name"  placeholder="Enter your Name"/>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="form-group">
-                                <label htmlFor="email" className="cols-sm-2 control-label">Your Email</label>
-                                <div className="cols-sm-10">
-                                    <div className="input-group">
-                                        <span className="input-group-addon"><i className="fa fa-envelope fa" aria-hidden="true"></i></span>
-                                        <input type="text" className="form-control" name="email" ref="email"  placeholder="Enter your Email"/>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="form-group">
-                                <label htmlFor="username" className="cols-sm-2 control-label">Username</label>
-                                <div className="cols-sm-10">
-                                    <div className="input-group">
-                                        <span className="input-group-addon"><i className="fa fa-users fa" aria-hidden="true"></i></span>
-                                        <input type="text" className="form-control" name="username" ref="username"  placeholder="Enter your Username"/>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="form-group">
-                                <label htmlFor="password" className="cols-sm-2 control-label">Password</label>
-                                <div className="cols-sm-10">
-                                    <div className="input-group">
-                                        <span className="input-group-addon"><i className="fa fa-lock fa-lg" aria-hidden="true"></i></span>
-                                        <input type="password" className="form-control" name="password" ref="password"  placeholder="Enter your Password"/>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="form-group">
-                                <label htmlFor="confirm" className="cols-sm-2 control-label">Confirm Password</label>
-                                <div className="cols-sm-10">
-                                    <div className="input-group">
-                                        <span className="input-group-addon"><i className="fa fa-lock fa-lg" aria-hidden="true"></i></span>
-                                        <input type="password" className="form-control" name="confirm" ref="confirm"  placeholder="Confirm your Password"/>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="form-group ">
-                                <button type="button" className="btn btn-primary btn-lg btn-block login-button" onClick={this.handleSubmit}>Register</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
         )
     }
 }
