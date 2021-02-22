b33d2ade8dab6d2f4dd66c1c83770e29da9c630b
Kolordus <kolak.jacek@gmail.com>
2021-02-22 20:03:47 +0100

    nowy commit

--- basic/src/main/java/com/koziolpaulina/foodmanager/auth/UserDetailsServiceImpl.java
+++ basic/src/main/java/com/koziolpaulina/foodmanager/auth/UserDetailsServiceImpl.java
@@ -27,6 +27,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         List<GrantedAuthority> grantedAuths = new ArrayList<>();
         grantedAuths.add(new SimpleGrantedAuthority("ROLE_USER"));
 
+        System.out.println("elo elo");
         return new org.springframework.security.core.userdetails.User(foundUser.getUsername(), foundUser.getPassword(), grantedAuths);
     }
 }
